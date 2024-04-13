6f0888f9835d5a4fe40026a315bc5e75f46a5122
byjun8932 <byjun8932@gmail.com>
2024-04-13 09:02:06 +0900

    feat: default webView Setting

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_webbrowser_240413/theme.dart';
+import 'dart:io';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -10,11 +12,20 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('나만의 웹브라우저'),
         backgroundColor: Theme.of(context).colorScheme.tertiaryContainer.tone(85),
+      ),
+      body: WebView(
+        initialUrl: 'https://www.pub.dev',
       ),
     );
   }
